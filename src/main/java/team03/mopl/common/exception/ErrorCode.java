a801aec4576dc1b6baf23d0e90ba80132a58691f
yudility <yudility@gmail.com>
2025-06-26 13:07:53 +0900

    feat: 존재하지 않은 채팅방 예외 및 에러코드 추가

--- src/main/java/team03/mopl/common/exception/ErrorCode.java
+++ src/main/java/team03/mopl/common/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
 
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "존재하지 않는 사용자입니다."),
   DUPLICATED_EMAIL(HttpStatus.CONFLICT, "USER_002", "이미 사용중인 이메일입니다"),
-  DUPLICATED_NAME(HttpStatus.CONFLICT, "USER_003", "이미 사용 중인 사용자명입니다.");
+  DUPLICATED_NAME(HttpStatus.CONFLICT, "USER_003", "이미 사용 중인 사용자명입니다."),
+
+  CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "CHATROOM_001", "존재하지 않는 채팅방입니다.");
 
   private HttpStatus status;
   // 추적하기 쉽도록하는 필드
