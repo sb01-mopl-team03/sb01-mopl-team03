f02cde6282be12a731ed673a22730ed3ea185a21
kyungrin Kim <0123jade0123@gmail.com>
2025-06-28 18:43:58 +0900

    fix: Spring Batch 자동 구성(application.yaml 등) 상쇄로 인한 @EnableBatchProcessing 제거

--- src/main/java/team03/mopl/domain/content/batch/common/BatchConfig.java
+++ src/main/java/team03/mopl/domain/content/batch/common/BatchConfig.java
@@ -1,12 +1,10 @@
 package team03.mopl.domain.content.batch.common;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
 
   @Bean
