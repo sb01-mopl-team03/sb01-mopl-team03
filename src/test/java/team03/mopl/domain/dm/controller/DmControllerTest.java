38a95f31aa2697c89bac9b3f537d3ce981c3c87a
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-07-04 10:40:03 +0900

    refactor: 테스트 코드 수정

--- src/test/java/team03/mopl/domain/dm/controller/DmControllerTest.java
+++ src/test/java/team03/mopl/domain/dm/controller/DmControllerTest.java
@@ -14,8 +14,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import team03.mopl.domain.dm.dto.DmDto;
 import team03.mopl.domain.dm.service.DmRoomService;
@@ -29,10 +29,10 @@ class DmControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
+  @MockitoBean
   private DmService dmService;
 
-  @MockBean
+  @MockitoBean
   private DmRoomService dmRoomService;
 
   private UUID roomId;
--- src/test/java/team03/mopl/domain/dm/controller/DmRoomControllerTest.java
+++ src/test/java/team03/mopl/domain/dm/controller/DmRoomControllerTest.java
@@ -16,8 +16,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import team03.mopl.domain.dm.dto.DmRoomDto;
 import team03.mopl.domain.dm.service.DmRoomService;
@@ -33,10 +33,10 @@ class DmRoomControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
+  @MockitoBean
   private DmRoomService dmRoomService;
 
-  @MockBean
+  @MockitoBean
   private DmService dmService;
 
   @Test
