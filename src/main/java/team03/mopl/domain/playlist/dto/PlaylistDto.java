bf4bde1bb6f351bb94e689597c35e724fa385df6
iiyubb <hiyubin.lee@gmail.com>
2025-07-10 09:59:20 +0900

    refactor: playlistDto에 id 필드 추가

--- src/main/java/team03/mopl/domain/playlist/dto/PlaylistDto.java
+++ src/main/java/team03/mopl/domain/playlist/dto/PlaylistDto.java
@@ -1,12 +1,14 @@
 package team03.mopl.domain.playlist.dto;
 
 import java.util.List;
+import java.util.UUID;
 import team03.mopl.domain.playlist.entity.Playlist;
 import team03.mopl.domain.playlist.entity.PlaylistContent;
 import team03.mopl.domain.subscription.Subscription;
 import team03.mopl.domain.user.User;
 
 public record PlaylistDto(
+    UUID playlistId,
     String name,
     User user,
     Boolean isPublic,
@@ -16,6 +18,7 @@ public record PlaylistDto(
 
   public static PlaylistDto from(Playlist playlist) {
     return new PlaylistDto(
+        playlist.getId(),
         playlist.getName(),
         playlist.getUser(),
         playlist.isPublic(),
