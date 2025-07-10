d944de78c9db41b3ba5ae7afca4ada77db5486d9
iiyubb <hiyubin.lee@gmail.com>
2025-07-10 16:40:24 +0900

    refactor: keywordId 필드 추가

--- src/main/java/team03/mopl/domain/curation/dto/KeywordDto.java
+++ src/main/java/team03/mopl/domain/curation/dto/KeywordDto.java
@@ -4,12 +4,14 @@ import java.util.UUID;
 import team03.mopl.domain.curation.entity.Keyword;
 
 public record KeywordDto(
+    UUID keywordId,
     UUID userId,
     String keyword
 ) {
 
   public static KeywordDto from(Keyword keyword) {
     return new KeywordDto(
+        keyword.getId(),
         keyword.getUser().getId(),
         keyword.getKeyword()
     );
