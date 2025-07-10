2d14ec13dba30926ca2580000aec0805ac05cb16
iiyubb <hiyubin.lee@gmail.com>
2025-07-10 13:21:07 +0900

    refactor: subscriptionDto에 subscriptionId 추가

--- src/main/java/team03/mopl/domain/subscription/dto/SubscriptionDto.java
+++ src/main/java/team03/mopl/domain/subscription/dto/SubscriptionDto.java
@@ -4,12 +4,14 @@ import java.util.UUID;
 import team03.mopl.domain.subscription.Subscription;
 
 public record SubscriptionDto(
+    UUID subscriptionId,
     UUID userId,
     UUID playlistId
 ) {
 
   public static SubscriptionDto from(Subscription subscription) {
     return new SubscriptionDto(
+        subscription.getId(),
         subscription.getUser().getId(),
         subscription.getPlaylist().getId()
     );
