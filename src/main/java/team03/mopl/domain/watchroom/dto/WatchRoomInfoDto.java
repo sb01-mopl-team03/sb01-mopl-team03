8435c715f1af713c8bc23c751ba7a8ef64758e6e
yudility <yudility@gmail.com>
2025-07-15 09:44:44 +0900

    fix: WatchRoomInfoDto에서 Setter 제거

--- src/main/java/team03/mopl/domain/watchroom/dto/WatchRoomInfoDto.java
+++ src/main/java/team03/mopl/domain/watchroom/dto/WatchRoomInfoDto.java
@@ -2,12 +2,10 @@ package team03.mopl.domain.watchroom.dto;
 
 import java.util.UUID;
 import lombok.Builder;
-import lombok.Setter;
 
 @Builder
 public record WatchRoomInfoDto (
     UUID id,
-    @Setter
     UUID newUserId,
     String contentTitle,
     ParticipantsInfoDto participantsInfoDto
