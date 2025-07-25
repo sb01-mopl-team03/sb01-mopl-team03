9f9df326025976a3f7663fdad41a0e913928ce1c
iiyubb <hiyubin.lee@gmail.com>
2025-07-25 11:51:40 +0900

    fix: spring data opensearch로 수정

--- src/main/java/team03/mopl/common/config/ElasticsearchConfig.java
+++ src/main/java/team03/mopl/common/config/ElasticsearchConfig.java
@@ -1,36 +1,36 @@
-package team03.mopl.common.config;
-
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("dev")
-public class ElasticsearchConfig {
-
-  @Value("${spring.elasticsearch.uris}")
-  private String elasticsearchUrl;
-
-  @Bean
-  public ElasticsearchClient elasticsearchClient() {
-    // RestClient 생성
-    RestClient restClient = RestClient.builder(
-        HttpHost.create(elasticsearchUrl)
-    ).build();
-
-    // Transport 생성 (Jackson JSON 매퍼 사용)
-    ElasticsearchTransport transport = new RestClientTransport(
-        restClient, new JacksonJsonpMapper()
-    );
-
-    // ElasticsearchClient 생성
-    return new ElasticsearchClient(transport);
-  }
-}
+//package team03.mopl.common.config;
+//
+//import co.elastic.clients.elasticsearch.ElasticsearchClient;
+//import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+//import co.elastic.clients.transport.ElasticsearchTransport;
+//import co.elastic.clients.transport.rest_client.RestClientTransport;
+//import org.apache.http.HttpHost;
+//import org.elasticsearch.client.RestClient;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//@Configuration
+//@Profile("dev")
+//public class ElasticsearchConfig {
+//
+//  @Value("${spring.elasticsearch.uris}")
+//  private String elasticsearchUrl;
+//
+//  @Bean
+//  public ElasticsearchClient elasticsearchClient() {
+//    // RestClient 생성
+//    RestClient restClient = RestClient.builder(
+//        HttpHost.create(elasticsearchUrl)
+//    ).build();
+//
+//    // Transport 생성 (Jackson JSON 매퍼 사용)
+//    ElasticsearchTransport transport = new RestClientTransport(
+//        restClient, new JacksonJsonpMapper()
+//    );
+//
+//    // ElasticsearchClient 생성
+//    return new ElasticsearchClient(transport);
+//  }
+//}
