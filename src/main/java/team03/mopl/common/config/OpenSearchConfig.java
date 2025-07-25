d8ac9efebf88f87bd310359ce59c0f6e903196be
iiyubb <hiyubin.lee@gmail.com>
2025-07-25 14:03:07 +0900

    fix: opensearch config 수정

--- src/main/java/team03/mopl/common/config/OpenSearchConfig.java
+++ src/main/java/team03/mopl/common/config/OpenSearchConfig.java
@@ -16,8 +16,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@Configuration
 @Profile("dev")
+@Configuration
 @EnableElasticsearchRepositories(basePackages = "team03.mopl.domain.curation.elasticsearch")
 public class OpenSearchConfig extends AbstractOpenSearchConfiguration {
 
